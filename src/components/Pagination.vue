9cec5356f720aa0cce1e44cbb4f975437eb43171
zhangxu <nikolazhang@163.com>
2019-09-18 12:58:36 +0800

    pagination

---
+++ src/components/Pagination.vue
@@ -0,0 +1,40 @@
+// 该组件使用方法参考
+// <uploadMine
+// ref="uploadMine"
+// :uploadMsg="communityUploadData"
+// v-on:update="receiveBannerUrl"
+// ></uploadMine>
+
+<template>
+  <div>
+    <div class="block">
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage"
+        :page-sizes="pagination.sizes"
+        :page-size="pagination.size"
+        :total="pagination.total"
+        layout="total, sizes, prev, pager, next, jumper"
+      ></el-pagination>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["pagination"],
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    }
+  },
+  data() {
+    return {
+        currentPage : 1
+    };
+  }
+};
+</script>
